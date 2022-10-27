06bf9d96eadba6b81b34b94544d51af5656c519d
Peter24Da <24er00@gmail.com>
2022-10-27 10:45:30 +0200

    draw function

--- etsk.js
+++ etsk.js
@@ -1,7 +1,23 @@
 let container = document.querySelector('#container');
-for (let i = 0; i < 256; i++) {
+
+
+for (let i = 0; i < 256; i++) {                     //default size
     let grid = document.createElement('div');
     grid.classList.add('grid');
 
     container.appendChild(grid);
+}
+
+let grids = document.getElementsByClassName("grid");
+
+let draw = function() {
+            this.style.backgroundColor = "red";
+            this.classList.add("colored")
+            let cucc = this.className;
+            console.log(typeof cucc);
+}
+    
+
+for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener('mouseenter', draw, false);
 }
