f2358eb9d0cb4a7b5958d2a129259406e2c1d66b
Peter24Da <24er00@gmail.com>
2023-02-01 19:57:48 +0100

    add change size

--- etsk.js
+++ etsk.js
@@ -1,23 +1,93 @@
 let container = document.querySelector('#container');
 
 
-for (let i = 0; i < 256; i++) {                     //default size
-    let grid = document.createElement('div');
-    grid.classList.add('grid');
 
-    container.appendChild(grid);
+
+let defaultSize = function(num){
+    for (let i = 0; i < num; i++) {                     
+        let grid = document.createElement('div');       
+        grid.classList.add('grid');                     
+                                                       
+        container.appendChild(grid);                    
+    }}
+    defaultSize(256);
+
+
+let rC = '';                                                       //
+let randomColor = function() {                                     //random color generator
+    rC = "#" + Math.floor(Math.random()*16777215).toString(16);    //
 }
 
-let grids = document.getElementsByClassName("grid");
+let coloring = function() {
+            randomColor();
 
-let draw = function() {
-            this.style.backgroundColor = "red";
+            if (this.className.includes('colored')) {
+                console.log(this.style.backgroundColor);
+            } else {
+                
+            }
+            this.style.backgroundColor = rC;
             this.classList.add("colored")
             let cucc = this.className;
             console.log(typeof cucc);
 }
     
 
+let grids = document.getElementsByClassName("grid");
+
+let draw = function(){                  
 for (let i = 0; i < grids.length; i++) {
-    grids[i].addEventListener('mouseenter', draw, false);
+    grids[i].addEventListener('mouseenter', coloring, false);
+}}
+draw();
+
+
+let clearGrid = function(){
+    for (let i = grids.length-1; i >= 0; i--) {
+        container.removeChild(grids[i]);
+    }
 }
+
+
+
+//change size
+
+let size = document.getElementById('size');
+
+let subButton = document.getElementById('subbutton');
+
+let containerSize = size.value*7;
+
+let changeContainerSize = function(){
+    container.classList.remove("dimension");
+    container.style.width = `${size.value*7}px`;
+    console.log('fdfs');
+    container.style.height = `${size.value}*7px`;
+    
+}
+let changeSize = function() {
+    clearGrid();
+    
+    if (size.value > 100) {
+        size.value = 100;
+    }
+    let newSize = size.value*size.value;
+    changeContainerSize();
+    defaultSize(newSize);
+    draw();
+}
+
+
+
+// Enter as apply button
+subButton.addEventListener('click', changeSize, false);
+let enter = function(){
+    console.log('enter')
+size.addEventListener("keyup", function(event){
+    if (event.key === 'Enter'){
+        event.preventDefault();
+        document.getElementById("subbutton").click();
+        size.blur();
+    }
+});}
+
